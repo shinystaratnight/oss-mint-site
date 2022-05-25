2c7df8eac437695caea66aaf4371959b327f4a97
Literally Collective <bluedragon.cstar@outlook.com>
2022-05-25 05:35:36 -0300

    Update Item.tsx

--- src/components/ConnectWallet/Item.tsx
+++ src/components/ConnectWallet/Item.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import * as Element from './styles';
+import React from "react";
+import * as Element from "./styles";
 
 interface Props {
   image: string;
   title: string;
+  handleClick: () => void;
 }
 
-const Item: React.FC<Props> = ({image, title}) => {
+const Item: React.FC<Props> = ({ image, title, handleClick }) => {
   return (
     <div className="col-6 mt-2">
-      <Element.ConnectItem href="#" className="d-flex flex-column align-items-center justify-content-between">
+      <Element.ConnectItem
+        href="#"
+        className="d-flex flex-column align-items-center justify-content-between"
+        onClick={handleClick}
+      >
         <img src={image} alt="" width={42} />
-        <Element.ConnectItemName className="mt-2">{title}</Element.ConnectItemName>
+        <Element.ConnectItemName className="mt-2">
+          {title}
+        </Element.ConnectItemName>
       </Element.ConnectItem>
     </div>
-  )
-}
+  );
+};
 
 export default Item;
