d4a4f6ac13976db466096313999f6ee3a3ab433a
Literally Collective <bluedragon.cstar@outlook.com>
2022-05-25 05:36:05 -0300

    Update index.tsx

--- src/components/ConnectWallet/index.tsx
+++ src/components/ConnectWallet/index.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
-import Item from './Item';
+import { ConnectorNames } from "config";
+import React from "react";
+import Item from "./Item";
 
-const ConnectWallet = () => {
+const ConnectWallet = ({ handleConnect }) => {
   return (
-     <React.Fragment>
-       <div className="container-fluid">
+    <React.Fragment>
+      <div className="container-fluid">
         <div className="row">
-          <Item image="/metamask.svg" title="Metamask" />
-          <Item image="/trustwallet.svg" title="TrustWallet" />
-          <Item image="/walletconnect.svg" title="WalletConnect" />
-          <Item image="/coinbase.png" title="Coinbase Wallet" />
-          <Item image="/safepal.svg" title="SafePal Wallet" />
-          <Item image="/paypal.png" title="TokenPocket" />
-          <Item image="/math.png" title="Math Wallet" />
+          <Item
+            image="/metamask.svg"
+            title="Metamask"
+            handleClick={() => handleConnect(ConnectorNames.Injected)}
+          />
+          <Item
+            image="/trustwallet.svg"
+            title="TrustWallet"
+            handleClick={() => handleConnect(ConnectorNames.Injected)}
+          />
+          <Item
+            image="/walletconnect.svg"
+            title="WalletConnect"
+            handleClick={() => handleConnect(ConnectorNames.WalletConnect)}
+          />
+          <Item
+            image="/coinbase.png"
+            title="Coinbase Wallet"
+            handleClick={() => handleConnect(ConnectorNames.Injected)}
+          />
+          <Item
+            image="/safepal.svg"
+            title="SafePal Wallet"
+            handleClick={() => handleConnect(ConnectorNames.Injected)}
+          />
+          <Item
+            image="/paypal.png"
+            title="TokenPocket"
+            handleClick={() => handleConnect(ConnectorNames.Injected)}
+          />
+          {/* <Item image="/math.png" title="Math Wallet" /> */}
         </div>
-       </div>
-     </React.Fragment>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default ConnectWallet;
