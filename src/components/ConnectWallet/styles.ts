c4eb84a32aef10eba57d726675f48126afa91ebd
Literally Collective <bluedragon.cstar@outlook.com>
2022-05-25 05:35:42 -0300

    Update styles.ts

--- src/components/ConnectWallet/styles.ts
+++ src/components/ConnectWallet/styles.ts
@@ -7,11 +7,12 @@ export const ConnectItem = styled.a`
   padding-top: 1rem;
   padding-bottom: 1rem;
   border-radius: 6px;
-  border: 2px solid var(--primary-color);
+  border: 2px solid rgb(37 37 66);
   color: var(--white-color);
 
   &:hover {
-    color: var(--primary-color)
+    color: var(--primary-color);
+    background: #090624;
   }
 `;
 
